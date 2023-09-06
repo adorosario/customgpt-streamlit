1ab7e4f9c130a7301ff638d058c0c5da05d53146
Alden Do Rosario <alden@chitika.com>
2023-09-05 23:41:16 -0400

    Cleaned up messaging and user flow

--- streamlitAndCustomGpt.py
+++ streamlitAndCustomGpt.py
@@ -3,16 +3,14 @@ import requests
 import os
 import json
 from sseclient import SSEClient
+import uuid
 
-
-api_endpoint = 'https://app.customgpt.ai/api/v1/'
-
-
+api_endpoint = 'https://dev.customgpt.ai/api/v1/'
 
 # Utilities
 
 def get_citations(api_token, project_id, citation_id):
-    url = "https://app.customgpt.ai/api/v1/projects/"+str(project_id)+"/citations/" +str(citation_id)
+    url = api_endpoint + "projects/"+str(project_id)+"/citations/" +str(citation_id)
 
     headers = {
         "accept": "application/json",
@@ -31,10 +29,16 @@ def get_citations(api_token, project_id, citation_id):
                     source = {'title': 'source', 'url': "" }
                 
             except:
-                if result['citation']['page_url'] != None:
-                    source = {'title': 'source', 'url': result['citation']['page_url'] }
-                else:
-                    source = {'title': 'source', 'url': "" }
+                try:
+                    if result['data']['page_url'] != None:
+                        source = {'title': result['data']['title'], 'url': result['data']['page_url'] }
+                    else:
+                        source = {'title': 'source', 'url': "" }
+                except:
+                    if result['citation']['page_url'] != None:
+                        source = {'title': 'source', 'url': result['citation']['page_url'] }
+                    else:
+                        source = {'title': 'source', 'url': "" }
             
             return source
         else:
@@ -42,26 +46,6 @@ def get_citations(api_token, project_id, citation_id):
     except requests.exceptions.RequestException as e:
         print(f"'erreur")
    
-def create_conversation(api_token, project_id, name):
-    url = api_endpoint + "projects/" + str(project_id) +"/conversations"
-
-    headers = {
-        "accept": "application/json",
-        "authorization": 'Bearer ' + api_token
-    }
-    payload = { "name": name }
-    try:
-        response = requests.post(url, json=payload, headers=headers)
-        response.raise_for_status()
-        result = json.loads(response.text)
-        if result['status'] == 'success':
-            return result['data']
-        else:
-            return json.loads(response.text)["data"]
-    except requests.exceptions.RequestException as e:
-        print(f"{e}")
-        return (False, "Error")
-
 def query_chatbot(api_token, project_id,session_id,message,stream='true', lang='en' ):
     url = api_endpoint + "projects/" + str(project_id) +"/conversations/"+str(session_id)+"/messages?stream="+str(stream)+"&lang="+str(lang)
 
@@ -80,30 +64,35 @@ def query_chatbot(api_token, project_id,session_id,message,stream='true', lang='
             resp_data = eval(event.data.replace('null', 'None'))
 
             if resp_data is not None:
+                if resp_data.get('status') == 'error':
+                    response.append(resp_data.get('message', ''))
+
                 if resp_data.get('status') == 'progress':
                     response.append(resp_data.get('message', ''))
 
                 if resp_data.get('status') == 'finish' and resp_data.get('citations') is not None:
                     citation_ids = resp_data.get('citations', [])
-                    citations = ""
+
+                    citation_links = []
+                    ccount = 1
                     for citation_id in citation_ids:
                         citation_obj = get_citations(api_token, project_id, citation_id)
-                        if len(citation_obj.get('url', '')) > 0:
-                            if citation_id == citation_ids[-1]:
-                                citations += citation_obj['url']
-                            else:
-                                citations += citation_obj['url'] + ", "
+                        url = citation_obj.get('url', '')
+                        
+                        if len(url) > 0:
+                            formatted_url = f"[{ccount}]({url})"
+                            ccount += 1
+                            citation_links.append(formatted_url)
 
-                    if len(citations) > 0:
-                        cita = "\n\n (Source: " + citations + " )"
+                    if citation_links:
+                        cita = "\n\nSources: " + ", ".join(citation_links)
                         response.append(cita)
-
         return response
     except requests.exceptions.RequestException as e:
         return ["Error"]
 
 def get_projectList(api_token):
-    url = "https://app.customgpt.ai/api/v1/projects?page=1&order=desc&width=100%25&height=auto"
+    url = api_endpoint + "projects?page=1&order=desc&width=100%25&height=auto"
 
     headers = {
         "accept": "application/json",
@@ -121,72 +110,33 @@ def get_projectList(api_token):
     except requests.exceptions.RequestException as e:
         print(f"'erreur")
 
-def get_conversationList(api_token, project_id):
-    url = "https://app.customgpt.ai/api/v1/projects/"+str(project_id) +"/conversations?page=1&order=desc&userFilter=all"
-
-    headers = {
-        "accept": "application/json",
-        "authorization": 'Bearer ' + api_token
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        result = json.loads(response.text)
-        if result['status'] == 'success':
-            return result['data']['data']
-        else:
-            return []
-    except requests.exceptions.RequestException as e:
-        print(f"'erreur")
-
-
-
+def clear_chat_history():
+    st.session_state.session_id = str(uuid.uuid4())
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
 # App title
 st.set_page_config(page_title="CustomGPT Chatbot")
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
 
-# Replicate Credentials
+# CustomGPT Credentials
 with st.sidebar:
     st.title('CustomGPT Chatbot')
    
-    replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    if not replicate_api:
-        st.warning('Please enter your credentials!', icon='⚠️')
-    else:
-        st.success('API KEY PROVIDED!', )
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    customgpt_api_key = st.text_input('Enter CustomGPT API Key:', type='password')
+    if customgpt_api_key:
 
-    st.subheader('Select a project')
-    listProject = get_projectList(replicate_api)
-    if listProject is not None:
-        projectNames = [projt['project_name'] for projt in listProject]
-        selected_model = st.sidebar.selectbox('', projectNames, key='selected_model')
-        index = projectNames.index(selected_model)
-        selected_project = listProject[index]
-        if selected_project:
-            st.subheader('Select a conversation')
-            listConversation = get_conversationList(replicate_api,selected_project['id'])
-            if listConversation is not None and len(listConversation) > 0:
-                convNames = [conv['name'] for conv in listConversation]
-                selected_conve = st.selectbox('', convNames, key='selected_conv')
-                indx = convNames.index(selected_conve)
-                selected_conv = listConversation[indx]
-
-                if selected_conv:
-                    st.success(f'Now you can start!!', icon='👉')
-            else:
-                st.warning('No conversations found for the selected project. You can create a new conversation below.', icon='⚠️')
-                new_conv_name = st.text_input('Enter a name for the new conversation:')
-                if st.button('Create New Conversation'):
-                    selected_conv = create_conversation(replicate_api,selected_project['id'],new_conv_name)
-                    st.success(f'Created a new conversation: {new_conv_name}', icon='✅')
+        st.subheader('Select Project')
+        listProject = get_projectList(customgpt_api_key)
+        if listProject is not None:
+            projectNames = [projt['project_name'] for projt in listProject]
+            selected_model = st.sidebar.selectbox('', projectNames, key='selected_model')
+            index = projectNames.index(selected_model)
+            selected_project = listProject[index]
         else:
-            st.error('No project selected.', icon='❌')
-    else:
-        st.error('No projects found. Check your API key.', icon='❌')
+            st.error('No projects found. Please check your API key.', icon='❌')
+        st.sidebar.button('Reset Chat', on_click=clear_chat_history)
 
-   
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
@@ -196,13 +146,8 @@ for index, message in enumerate(st.session_state.messages):
         # Display the message content
         st.write(f"{message['content']}")
 
-        
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
 # User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
+if prompt := st.chat_input(disabled=not customgpt_api_key):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         # Display the user's message
@@ -212,13 +157,15 @@ if prompt := st.chat_input(disabled=not replicate_api):
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = query_chatbot(replicate_api, selected_conv['project_id'], selected_conv['session_id'], prompt)
+            response = query_chatbot(customgpt_api_key, selected_project['id'], st.session_state.session_id, prompt)
             placeholder = st.empty()
             full_response = ""
 
             for item in response:
                 full_response += item
                 placeholder.markdown(full_response)
-
+    if full_response == "":
+        full_response = "Oh no! Any unknown error has occured. Please check your CustomGPT Dashboard for details."
+        placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
